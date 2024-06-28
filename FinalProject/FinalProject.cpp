8ff264f041b7abbcff5183793d508a53869a95df
d1ctat0rr <738494482@qq.com>
2024-06-29 00:57:52 +0800

    Update FinalProject.cpp

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -9,6 +9,8 @@
 #define new DEBUG_NEW
 #endif
 
+
+//华南理工大学软件工程专业中望CAD实训课程结题项目————扫雷游戏-黄玉米版 SCUT ZWCAD Practical training courses Final project ——— MineClearance - HuangYuning version
 //
 //TODO: If this DLL is dynamically linked against the MFC DLLs,
 //		any functions exported from this DLL which call into
